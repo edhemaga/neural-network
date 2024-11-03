b57e4ce4da5273cb53fd0069b57648541606276f
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-11-03 21:55:47 +0100

    init

---
+++ script.js
@@ -0,0 +1,75 @@
+class Layer {
+	input;
+	output;
+
+	weights;
+	biases;
+
+	constructor(input, outputSize) {
+		this.input = input;
+		this.initializeOutputs(outputSize);
+		this.initializeWeights();
+	}
+
+	initializeOutputs(outputSize) {
+		this.output = [];
+		for (let i = 0; i < outputSize; i++) {
+			this.output = [...this.output, 0];
+		}
+	}
+
+	initializeWeights() {
+		this.weights = [];
+		for (let i = 0; i < this.input?.length; i++) {
+			this.weights = [...this.weights, []];
+
+			for (let j = 0; j < this.output?.length; j++) {
+				const rnd = Math.random() - 0.5;
+				this.weights[i] = [
+					...this.weights[i],
+					Math.round(rnd * 1000) / 1000,
+				];
+			}
+		}
+	}
+}
+
+class Network {
+	// TODO add some input
+	layers;
+
+	constructor(input, hiddenLayersOutputSize) {
+		this.layers = [new Layer(input, 5)];
+
+		//
+		hiddenLayersOutputSize.forEach((outputSize) => {
+			const length = this.layers?.length - 1;
+			this.layers = [
+				...this.layers,
+				new Layer(this.layers[length]?.output, outputSize),
+			];
+		});
+
+		this.layers.forEach((layer) => {
+			this.feedforward(layer);
+		});
+	}
+
+	feedforward(layer) {
+		for (let i = 0; i < layer.input?.length; i++) {
+			for (let j = 0; j < layer.output?.length; j++) {
+				layer.output[j] +=
+					Math.round(layer.weights[i][j] * layer.input[i] * 1000) /
+					1000;
+			}
+		}
+	}
+
+	sigmoid(x) {
+		return 1 / (1 + Math.exp(-x));
+	}
+}
+
+var network = new Network([1, 2, 3], [5, 2]);
+
+console.log(network);
